692f5b972e26fa389547f8e01dbec8f3cf78c026
NicolayDodonov <95965202+NicolayDodonov@users.noreply.github.com>
2025-10-06 18:45:50 +0400

    Update 251006_start.sql

---
+++ migration/251006_start.sql
@@ -0,0 +1,38 @@
+BEGIN
+
+create table if not exists user_type
+(
+    id   uuid default gen_random_uuid() not null
+    constraint user_type_pk
+    primary key,
+    type text
+    );
+
+create table if not exists user_data
+(
+    id              uuid not null
+    constraint user_data_pk
+    primary key,
+    count_read_book integer,
+    reader_score    integer,
+    favorite_book   text[][]
+);
+
+create table if not exists users
+(
+    id         uuid default gen_random_uuid() not null
+    constraint users_pk
+    primary key,
+    first_name text,
+    last_name  text,
+    user_type  uuid
+    constraint users_user_type_id_fk
+    references user_type,
+    login      text,
+    password   text,
+    data       uuid
+    constraint users_user_data_id_fk
+    references user_data
+    );
+
+COMMIT;
